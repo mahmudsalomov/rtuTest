da3d8691380844c35ece8624211c5a1f0e00a291
Mahmud Salomov <mahmud.salomov.salomov@gmail.com>
2024-03-30 18:40:19 +0500

    test

--- src/main/java/uz/maniac4j/forwarder.py
+++ src/main/java/uz/maniac4j/forwarder.py
@@ -6,9 +6,9 @@ from serial import Serial
 
 
 def run_serial_forwarder():
-    client = ModbusClient(method="rtu", port="COM2", stopbits=1, bytesize=8, parity='N', baudrate=19200)
+    client = ModbusClient(method="rtu", port="COM4", stopbits=1, bytesize=8, parity='N', baudrate=9600)
 
-    store = RemoteSlaveContext(client, slave=1)
+    store = RemoteSlaveContext(client, slave=12)
     store3 = RemoteSlaveContext(client, slave=3)
     context = ModbusServerContext(slaves={1: store, 3: store3}, single=False)
 
